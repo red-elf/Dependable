0a4186a8e0ebb9c3892b9cd2a971dfcf8d5e4063
Milos Vasic <milos85vasic@gmail.com>
2022-07-27 18:25:20 +0300

    Working on the dependencies installation script.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -15,17 +15,24 @@ if test -e "$DEPENDENCIES"; then
   echo "Installing the dependencies"
   for i in "$DEPENDENCIES"/*.sh; do
 
-      # shellcheck disable=SC1090
-      . "$i"
-      echo "Dependency: $i"
-      echo "Dependency repository: $DEPENDABLE_REPOSITORY"
-      echo "Dependency branch: $DEPENDABLE_BRANCH"
-      echo "Dependency tag: $DEPENDABLE_TAG"
+    # shellcheck disable=SC1090
+    . "$i"
+    echo "Dependency: $i"
+    echo "Dependency repository: $DEPENDABLE_REPOSITORY"
+    echo "Dependency branch: $DEPENDABLE_BRANCH"
+    echo "Dependency tag: $DEPENDABLE_TAG"
 
-      if ! test -e "$DEPENDENCIES_WORKING_DIRECTORY"; then
+    DEPENDABLE_WORKING_DIRECTORY="${i%.*}"
+    echo "Dependency working directory: $DEPENDABLE_WORKING_DIRECTORY"
 
-        mkdir "$DEPENDENCIES_WORKING_DIRECTORY"
-      fi
+    WORKING_DIRECTORY="$DEPENDENCIES_WORKING_DIRECTORY/$DEPENDABLE_WORKING_DIRECTORY"
+    if ! test -e "$WORKING_DIRECTORY"; then
+
+      mkdir -p "$WORKING_DIRECTORY"
+    else
+
+      echo "Directory already exists: $WORKING_DIRECTORY"
+    fi
   done
 else
 
