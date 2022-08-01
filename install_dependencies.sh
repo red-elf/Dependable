39bf9e90877c9bf890c1f5fcdfaa139d632a55ac
Milos Vasic <milos85vasic@gmail.com>
2022-08-01 20:31:55 +0300

    FIXME added.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -180,6 +180,8 @@ if test -e "$DEPENDENCIES"; then
         echo "Installed:"
         echo "$INSTALLED"
 
+        # FIXME: Iterate through the installed!
+
         if [[ "$INSTALLED" == "$CURRENT" ]] && ! [[ "$CURRENT" == *"-SNAPSHOT"* ]] ; then
 
           echo "The '$i' is already installed, version: $CURRENT"
